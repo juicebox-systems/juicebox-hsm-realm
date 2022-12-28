208538dca249e2f45d2315dcfc897041745d1867
Diego Ongaro <diego@loam.me>
2022-12-28 15:58:19 -0800

    voprf_benchmark: Benchmark Verifiable OPRF too
    Results from Diego's Intel laptop:
    
    ```
    random/OsRng            time:   [188.73 ns 189.21 ns 189.77 ns]
                            thrpt:  [5.2695 Melem/s 5.2853 Melem/s 5.2985 Melem/s]
    random/ChaCha RNG       time:   [13.590 ns 13.621 ns 13.653 ns]
                            thrpt:  [73.247 Melem/s 73.416 Melem/s 73.585 Melem/s]
    
    unverifiable/client blind
                            time:   [39.304 µs 39.337 µs 39.371 µs]
                            thrpt:  [25.400 Kelem/s 25.421 Kelem/s 25.443 Kelem/s]
    unverifiable/server evaluate
                            time:   [29.804 µs 29.869 µs 29.948 µs]
                            thrpt:  [33.391 Kelem/s 33.479 Kelem/s 33.553 Kelem/s]
    unverifiable/client unblind
                            time:   [44.089 µs 44.139 µs 44.201 µs]
                            thrpt:  [22.624 Kelem/s 22.656 Kelem/s 22.681 Kelem/s]
    unverifiable/OPRF       time:   [115.74 µs 115.89 µs 116.02 µs]
                            thrpt:  [8.6191 Kelem/s 8.6290 Kelem/s 8.6404 Kelem/s]
    
    verifiable/client blind time:   [38.214 µs 38.261 µs 38.308 µs]
                            thrpt:  [26.104 Kelem/s 26.137 Kelem/s 26.168 Kelem/s]
    verifiable/server evaluate
                            time:   [187.50 µs 187.70 µs 187.91 µs]
                            thrpt:  [5.3216 Kelem/s 5.3278 Kelem/s 5.3332 Kelem/s]
    verifiable/client unblind & verify
                            time:   [263.70 µs 263.87 µs 264.05 µs]
                            thrpt:  [3.7872 Kelem/s 3.7897 Kelem/s 3.7921 Kelem/s]
    verifiable/VOPRF        time:   [520.41 µs 521.10 µs 521.80 µs]
                            thrpt:  [1.9165 Kelem/s 1.9190 Kelem/s 1.9216 Kelem/s]
    ```

--- benches/voprf_benchmark.rs
+++ benches/voprf_benchmark.rs
@@ -5,85 +5,197 @@ use rand::{CryptoRng, Rng, SeedableRng};
 use rand_chacha::ChaCha12Rng;
 
 type CipherSuite = voprf::Ristretto255;
-type OprfClient = voprf::OprfClient<CipherSuite>;
-type OprfServer = voprf::OprfServer<CipherSuite>;
 type OprfResult = digest::Output<<CipherSuite as voprf::CipherSuite>::Hash>;
+type BlindedElement = voprf::BlindedElement<CipherSuite>;
+type EvaluationElement = voprf::EvaluationElement<CipherSuite>;
 
-fn random<R: Rng + CryptoRng>(rng: &mut R) -> [u8; 32] {
-    let mut result = [0u8; 32];
-    rng.fill(&mut result);
-    result
+mod random {
+    use super::*;
+
+    fn random<R: Rng + CryptoRng>(rng: &mut R) -> [u8; 32] {
+        let mut result = [0u8; 32];
+        rng.fill(&mut result);
+        result
+    }
+
+    pub fn benchmark(c: &mut Criterion) {
+        let mut group = c.benchmark_group("random");
+        group.throughput(criterion::Throughput::Elements(1));
+
+        group.bench_function("OsRng", |b| b.iter(|| random(&mut OsRng)));
+        group.bench_function("ChaCha RNG", |b| {
+            let mut rng = ChaCha12Rng::seed_from_u64(7);
+            b.iter(|| random(&mut rng))
+        });
+    }
 }
 
-fn client_blind<R: Rng + CryptoRng>(
-    input: &[u8],
-    rng: &mut R,
-) -> voprf::OprfClientBlindResult<CipherSuite> {
-    OprfClient::blind(input, rng).unwrap()
-}
+mod unverifiable {
+    use super::*;
+    type Client = voprf::OprfClient<CipherSuite>;
+    type Server = voprf::OprfServer<CipherSuite>;
+    type ClientBlindResult = voprf::OprfClientBlindResult<CipherSuite>;
 
-fn server_evaluate(
-    blinded_input: &voprf::BlindedElement<CipherSuite>,
-) -> voprf::EvaluationElement<CipherSuite> {
-    let key = hex!("98ea6e4f216f2fb4b69fff9b3a44842c38686ca685f3f55dc48c5d3fb1107be4");
-    let oprf = OprfServer::new_from_seed(&key, &[]).unwrap();
-    oprf.blind_evaluate(blinded_input)
-}
+    fn client_blind<R: Rng + CryptoRng>(input: &[u8], rng: &mut R) -> ClientBlindResult {
+        Client::blind(input, rng).unwrap()
+    }
 
-fn client_unblind(
-    input: &[u8],
-    state: &voprf::OprfClient<CipherSuite>,
-    blinded_result: &voprf::EvaluationElement<CipherSuite>,
-) -> OprfResult {
-    state.finalize(input, blinded_result).unwrap()
-}
+    fn server_evaluate(blinded_input: &BlindedElement) -> EvaluationElement {
+        let key = hex!("98ea6e4f216f2fb4b69fff9b3a44842c38686ca685f3f55dc48c5d3fb1107be4");
+        let server = Server::new_from_seed(&key, &[]).unwrap();
+        server.blind_evaluate(blinded_input)
+    }
 
-fn oprf<R: Rng + CryptoRng>(input: &[u8], rng: &mut R) -> OprfResult {
-    let blinded_input = client_blind(input, rng);
-    let blinded_result = server_evaluate(&blinded_input.message);
-    client_unblind(input, &blinded_input.state, &blinded_result)
-}
+    fn client_unblind(
+        input: &[u8],
+        state: &Client,
+        blinded_result: &EvaluationElement,
+    ) -> OprfResult {
+        state.finalize(input, blinded_result).unwrap()
+    }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("voprf");
-    group.throughput(criterion::Throughput::Elements(1));
-
-    group.bench_function("random: OsRng", |b| b.iter(|| random(&mut OsRng)));
-    group.bench_function("random: ChaCha RNG", |b| {
-        let mut rng = ChaCha12Rng::seed_from_u64(7);
-        b.iter(|| random(&mut rng))
-    });
-
-    group.bench_function("client blind", |b| {
-        let mut rng = ChaCha12Rng::seed_from_u64(7);
-        b.iter(|| client_blind(black_box(b"1234"), &mut rng))
-    });
-
-    group.bench_function("server evaluate", |b| {
-        let blinded_input = client_blind(b"1234", &mut OsRng).message;
-        b.iter(|| server_evaluate(black_box(&blinded_input)));
-    });
-
-    group.bench_function("client unblind", |b| {
-        let input = b"1234";
-        let blinded_input = client_blind(input, &mut OsRng);
+    fn oprf<R: Rng + CryptoRng>(input: &[u8], rng: &mut R) -> OprfResult {
+        let blinded_input = client_blind(input, rng);
         let blinded_result = server_evaluate(&blinded_input.message);
-        b.iter(|| {
-            client_unblind(
-                black_box(input),
-                black_box(&blinded_input.state),
-                black_box(&blinded_result),
-            )
-        })
-    });
+        client_unblind(input, &blinded_input.state, &blinded_result)
+    }
 
-    group.bench_function("oprf", |b| {
-        let mut rng = ChaCha12Rng::seed_from_u64(7);
-        b.iter(|| oprf(black_box(b"1234"), &mut rng))
-    });
+    pub fn benchmark(c: &mut Criterion) {
+        let mut group = c.benchmark_group("unverifiable");
+        group.throughput(criterion::Throughput::Elements(1));
 
-    group.finish();
+        group.bench_function("client blind", |b| {
+            let mut rng = ChaCha12Rng::seed_from_u64(7);
+            b.iter(|| client_blind(black_box(b"1234"), &mut rng))
+        });
+
+        group.bench_function("server evaluate", |b| {
+            let blinded_input = client_blind(b"1234", &mut OsRng).message;
+            b.iter(|| server_evaluate(black_box(&blinded_input)));
+        });
+
+        group.bench_function("client unblind", |b| {
+            let input = b"1234";
+            let blinded_input = client_blind(input, &mut OsRng);
+            let blinded_result = server_evaluate(&blinded_input.message);
+            b.iter(|| {
+                client_unblind(
+                    black_box(input),
+                    black_box(&blinded_input.state),
+                    black_box(&blinded_result),
+                )
+            })
+        });
+
+        group.bench_function("OPRF", |b| {
+            let mut rng = ChaCha12Rng::seed_from_u64(7);
+            b.iter(|| oprf(black_box(b"1234"), &mut rng))
+        });
+    }
 }
 
-criterion_group!(benches, criterion_benchmark);
+mod verifiable {
+    use super::*;
+    type Client = voprf::VoprfClient<CipherSuite>;
+    type Server = voprf::VoprfServer<CipherSuite>;
+    type ClientBlindResult = voprf::VoprfClientBlindResult<CipherSuite>;
+    type ServerEvaluateResult = voprf::VoprfServerEvaluateResult<CipherSuite>;
+    type Proof = voprf::Proof<CipherSuite>;
+    type PublicKey = <<CipherSuite as voprf::CipherSuite>::Group as voprf::Group>::Elem;
+
+    fn client_blind<R: Rng + CryptoRng>(input: &[u8], rng: &mut R) -> ClientBlindResult {
+        Client::blind(input, rng).unwrap()
+    }
+
+    fn make_server() -> Server {
+        let key = hex!("98ea6e4f216f2fb4b69fff9b3a44842c38686ca685f3f55dc48c5d3fb1107be4");
+        Server::new_from_seed(&key, &[]).unwrap()
+    }
+
+    fn server_evaluate<R: Rng + CryptoRng>(
+        server: &Server,
+        blinded_input: &BlindedElement,
+        rng: &mut R,
+    ) -> ServerEvaluateResult {
+        server.blind_evaluate(rng, blinded_input)
+    }
+
+    fn client_unblind_and_verify(
+        input: &[u8],
+        state: &Client,
+        blinded_result: &EvaluationElement,
+        proof: &Proof,
+        public_key: PublicKey,
+    ) -> OprfResult {
+        state
+            .finalize(input, blinded_result, proof, public_key)
+            .unwrap()
+    }
+
+    pub fn run_voprf<R: Rng + CryptoRng>(input: &[u8], rng: &mut R) -> OprfResult {
+        let blinded_input = client_blind(input, rng);
+
+        let server = make_server();
+        let ServerEvaluateResult {
+            message: blinded_result,
+            proof,
+        } = server_evaluate(&server, &blinded_input.message, rng);
+
+        let public_key = server.get_public_key();
+        client_unblind_and_verify(
+            input,
+            &blinded_input.state,
+            &blinded_result,
+            &proof,
+            public_key,
+        )
+    }
+
+    pub fn benchmark(c: &mut Criterion) {
+        let mut group = c.benchmark_group("verifiable");
+        group.throughput(criterion::Throughput::Elements(1));
+
+        group.bench_function("client blind", |b| {
+            let mut rng = ChaCha12Rng::seed_from_u64(7);
+            b.iter(|| client_blind(black_box(b"1234"), &mut rng))
+        });
+
+        group.bench_function("server evaluate", |b| {
+            let mut rng = ChaCha12Rng::seed_from_u64(7);
+            let server = make_server();
+            let blinded_input = client_blind(b"1234", &mut rng).message;
+            b.iter(|| server_evaluate(black_box(&server), black_box(&blinded_input), &mut rng));
+        });
+
+        group.bench_function("client unblind & verify", |b| {
+            let input = b"1234";
+            let mut rng = ChaCha12Rng::seed_from_u64(7);
+            let blinded_input = client_blind(input, &mut rng);
+            let server = make_server();
+            let blinded_result = server_evaluate(&server, &blinded_input.message, &mut rng);
+            let public_key = server.get_public_key();
+            b.iter(|| {
+                client_unblind_and_verify(
+                    black_box(input),
+                    black_box(&blinded_input.state),
+                    black_box(&blinded_result.message),
+                    black_box(&blinded_result.proof),
+                    black_box(public_key),
+                )
+            })
+        });
+
+        group.bench_function("VOPRF", |b| {
+            let mut rng = ChaCha12Rng::seed_from_u64(7);
+            b.iter(|| run_voprf(black_box(b"1234"), &mut rng))
+        });
+    }
+}
+
+criterion_group!(
+    benches,
+    random::benchmark,
+    unverifiable::benchmark,
+    verifiable::benchmark
+);
 criterion_main!(benches);
