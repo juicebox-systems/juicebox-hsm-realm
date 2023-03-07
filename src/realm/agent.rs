753451575cdfc325acba50df60db4076f391e0f5
Diego Ongaro <diego@loam.me>
2023-03-06 17:55:25 -0800

    agent: retry on TreeStoreError::MissingNode

--- src/realm/agent.rs
+++ src/realm/agent.rs
@@ -1064,7 +1064,10 @@ async fn start_app_request<T: Transport>(
         .await
         {
             Ok(proof) => proof,
-            Err(TreeStoreError::MissingNode) => todo!(),
+            Err(TreeStoreError::MissingNode) => {
+                warn!(agent = name, "missing node, retrying");
+                continue;
+            }
             Err(TreeStoreError::Network(e)) => {
                 warn!(error = ?e, "start_app_request: error reading proof");
                 return Err(Response::NoStore);
@@ -1085,7 +1088,10 @@ async fn start_app_request<T: Transport>(
             Err(_) => return Err(Response::NoHsm),
             Ok(HsmResponse::InvalidRealm) => return Err(Response::InvalidRealm),
             Ok(HsmResponse::InvalidGroup) => return Err(Response::InvalidGroup),
-            Ok(HsmResponse::StaleProof) => continue,
+            Ok(HsmResponse::StaleProof) => {
+                warn!(agent = name, "stale proof, retrying");
+                continue;
+            }
             Ok(HsmResponse::NotLeader | HsmResponse::NotOwner) => return Err(Response::NotLeader),
             Ok(HsmResponse::InvalidProof) => return Err(Response::InvalidProof),
             // TODO, is this right? if we can't decrypt the leaf, then the proof is likely bogus.
