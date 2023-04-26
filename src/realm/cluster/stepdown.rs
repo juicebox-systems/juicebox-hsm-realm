4221daf954374f4718604ce2dca4079f66a8256a
simon <simon@loam.me>
2023-04-26 14:08:08 -0700

    breakup cluster.rs

---
+++ src/realm/cluster/stepdown.rs
@@ -0,0 +1,194 @@
+use futures::future::join_all;
+use futures::FutureExt;
+use std::collections::HashMap;
+use std::iter::zip;
+use tracing::{info, warn};
+use url::Url;
+
+use super::super::agent::types as agent_types;
+use super::super::rpc::HandlerError;
+use super::{types, ManagementGrant, Manager};
+use hsm_types::{Configuration, GroupId, HsmId, LogIndex};
+use hsmcore::hsm::types as hsm_types;
+use loam_sdk_core::types::RealmId;
+use loam_sdk_networking::rpc::{self, RpcError};
+
+impl Manager {
+    pub(super) async fn handle_leader_stepdown(
+        &self,
+        req: types::StepDownRequest,
+    ) -> Result<types::StepDownResponse, HandlerError> {
+        type Response = types::StepDownResponse;
+
+        let addresses: HashMap<HsmId, Url> = match self.0.store.get_addresses().await {
+            Ok(a) => a.into_iter().collect(),
+            Err(_err) => return Ok(Response::NoStore),
+        };
+
+        // calculate the exact set of step downs needed.
+        let stepdowns = match self.resolve_stepdowns(&req, &addresses).await {
+            Err(e) => return Ok(e),
+            Ok(sd) => sd,
+        };
+        let mut grants = Vec::with_capacity(stepdowns.len());
+        for stepdown in &stepdowns {
+            match self.mark_as_busy(stepdown.realm, stepdown.group) {
+                None => {
+                    return Ok(Response::Busy {
+                        realm: stepdown.realm,
+                        group: stepdown.group,
+                    })
+                }
+                Some(grant) => grants.push(grant),
+            }
+        }
+
+        for (stepdown, grant) in zip(stepdowns, grants) {
+            info!(url=%stepdown.url, hsm=?stepdown.hsm, group=?stepdown.group, realm=?stepdown.realm, "Asking Agent/HSM to step down as leader");
+            match rpc::send(
+                &self.0.agents,
+                &stepdown.url,
+                agent_types::StepDownRequest {
+                    realm: stepdown.realm,
+                    group: stepdown.group,
+                },
+            )
+            .await
+            {
+                Err(err) => return Ok(Response::RpcError(err)),
+                Ok(agent_types::StepDownResponse::NoHsm) => return Ok(Response::NoHsm),
+                Ok(agent_types::StepDownResponse::InvalidGroup) => {
+                    return Ok(Response::InvalidGroup)
+                }
+                Ok(agent_types::StepDownResponse::InvalidRealm) => {
+                    return Ok(Response::InvalidRealm)
+                }
+                Ok(agent_types::StepDownResponse::NotLeader) => return Ok(Response::NotLeader),
+                Ok(agent_types::StepDownResponse::Ok { last }) => {
+                    if let Err(err) = self
+                        .assign_leader_post_stepdown(&addresses, &grant, stepdown, Some(last))
+                        .await
+                    {
+                        return Ok(Response::RpcError(err));
+                    }
+                }
+            }
+        }
+        Ok(Response::Ok)
+    }
+
+    /// Leader stepdown was completed, assign a new one.
+    async fn assign_leader_post_stepdown(
+        &self,
+        addresses: &HashMap<HsmId, Url>,
+        grant: &ManagementGrant<'_>,
+        stepdown: Stepdown,
+        last: Option<LogIndex>,
+    ) -> Result<Option<HsmId>, RpcError> {
+        let hsm_status = super::get_hsm_statuses(
+            &self.0.agents,
+            stepdown
+                .config
+                .0
+                .iter()
+                .filter_map(|hsm| addresses.get(hsm)),
+        )
+        .await;
+
+        super::leader::assign_group_a_leader(
+            &self.0.agents,
+            grant,
+            stepdown.config,
+            Some(stepdown.hsm),
+            &hsm_status,
+            last,
+        )
+        .await
+    }
+
+    async fn resolve_stepdowns(
+        &self,
+        req: &types::StepDownRequest,
+        addresses: &HashMap<HsmId, Url>,
+    ) -> Result<Vec<Stepdown>, types::StepDownResponse> {
+        match req {
+            types::StepDownRequest::Hsm(hsm) => match addresses.get(hsm) {
+                None => {
+                    warn!(?hsm, "failed to find hsm in service discovery");
+                    Err(types::StepDownResponse::InvalidHsm)
+                }
+                Some(url) => {
+                    match rpc::send(&self.0.agents, url, agent_types::StatusRequest {}).await {
+                        Err(err) => {
+                            warn!(?err, url=%url, hsm=?hsm, "failed to get status of HSM");
+                            Err(types::StepDownResponse::RpcError(err))
+                        }
+                        Ok(agent_types::StatusResponse {
+                            hsm:
+                                Some(hsm_types::StatusResponse {
+                                    id,
+                                    realm: Some(rs),
+                                    ..
+                                }),
+                        }) if id == *hsm => Ok(rs
+                            .groups
+                            .into_iter()
+                            .filter_map(|gs| {
+                                gs.leader.map(|_| Stepdown {
+                                    hsm: *hsm,
+                                    url: url.clone(),
+                                    group: gs.id,
+                                    realm: rs.id,
+                                    config: gs.configuration,
+                                })
+                            })
+                            .collect()),
+                        Ok(_s) => {
+                            info!(?hsm,url=%url, "hsm is not a member of a realm");
+                            Ok(Vec::new())
+                        }
+                    }
+                }
+            },
+            types::StepDownRequest::Group { realm, group } => {
+                Ok(join_all(addresses.iter().map(|(_hsm, url)| {
+                    rpc::send(&self.0.agents, url, agent_types::StatusRequest {})
+                        .map(|r| (r, url.clone()))
+                }))
+                .await
+                .into_iter()
+                .filter_map(|(s, url)| {
+                    if let Some((hsm_id, realm_status)) = s
+                        .ok()
+                        .and_then(|s| s.hsm)
+                        .and_then(|hsm| hsm.realm.map(|r| (hsm.id, r)))
+                    {
+                        if realm_status.id == *realm {
+                            for g in realm_status.groups {
+                                if g.id == *group && g.leader.is_some() {
+                                    return Some(Stepdown {
+                                        hsm: hsm_id,
+                                        url,
+                                        group: *group,
+                                        realm: *realm,
+                                        config: g.configuration,
+                                    });
+                                }
+                            }
+                        }
+                    }
+                    None
+                })
+                .collect())
+            }
+        }
+    }
+}
+
+struct Stepdown {
+    hsm: HsmId,
+    url: Url,
+    group: GroupId,
+    realm: RealmId,
+    config: Configuration,
+}
---
+++ src/realm/cluster/transfer.rs
@@ -0,0 +1,172 @@
+use std::time::Duration;
+use tokio::time::sleep;
+use tracing::info;
+
+use super::super::super::http_client::{Client, ClientOptions};
+use super::super::agent::types::{
+    CompleteTransferRequest, CompleteTransferResponse, TransferInRequest, TransferInResponse,
+    TransferNonceRequest, TransferNonceResponse, TransferOutRequest, TransferOutResponse,
+    TransferStatementRequest, TransferStatementResponse,
+};
+use super::super::store::bigtable::StoreClient;
+use hsm_types::{GroupId, OwnedRange};
+use hsmcore::hsm::types as hsm_types;
+use loam_sdk_core::types::RealmId;
+use loam_sdk_networking::rpc;
+
+#[derive(Debug)]
+pub enum TransferError {
+    NoSourceLeader,
+    NoDestinationLeader,
+    // TODO: more error cases hidden in todo!()s.
+}
+
+pub async fn transfer(
+    realm: RealmId,
+    source: GroupId,
+    destination: GroupId,
+    range: OwnedRange,
+    store: &StoreClient,
+) -> Result<(), TransferError> {
+    type Error = TransferError;
+
+    info!(
+        ?realm,
+        ?source,
+        ?destination,
+        ?range,
+        "transferring ownership"
+    );
+
+    let agent_client = Client::new(ClientOptions::default());
+
+    let leaders = super::leader::find_leaders(store, &agent_client)
+        .await
+        .expect("TODO");
+
+    let Some((_, source_leader)) = leaders.get(&(realm, source)) else {
+        return Err(Error::NoSourceLeader);
+    };
+
+    let Some((_, dest_leader)) = leaders.get(&(realm, destination)) else {
+        return Err(Error::NoDestinationLeader);
+    };
+
+    // The current ownership transfer protocol is dangerous in that the moment
+    // the source group commits the log entry that the prefix is transferring
+    // out, the prefix must then move to the destination group. However, we
+    // don't have any guarantee that the destination group will accept the
+    // prefix. This is an issue with splitting in half: the only group that can
+    // accept a prefix is one that owns no prefix or one that owns the
+    // complementary prefix (the one with its least significant bit flipped).
+
+    let transferring_partition = match rpc::send(
+        &agent_client,
+        source_leader,
+        TransferOutRequest {
+            realm,
+            source,
+            destination,
+            range: range.clone(),
+        },
+    )
+    .await
+    {
+        Err(e) => todo!("{e:?}"),
+        Ok(TransferOutResponse::Ok { transferring }) => transferring,
+        Ok(r) => todo!("{r:?}"),
+    };
+
+    let nonce = match rpc::send(
+        &agent_client,
+        dest_leader,
+        TransferNonceRequest { realm, destination },
+    )
+    .await
+    {
+        Err(e) => todo!("{e:?}"),
+        Ok(TransferNonceResponse::Ok(nonce)) => nonce,
+        Ok(r) => todo!("{r:?}"),
+    };
+
+    let statement = match rpc::send(
+        &agent_client,
+        source_leader,
+        TransferStatementRequest {
+            realm,
+            source,
+            destination,
+            nonce,
+        },
+    )
+    .await
+    {
+        Err(e) => todo!("{e:?}"),
+        Ok(TransferStatementResponse::Ok(statement)) => statement,
+        Ok(r) => todo!("{r:?}"),
+    };
+
+    let dest_index = match rpc::send(
+        &agent_client,
+        dest_leader,
+        TransferInRequest {
+            realm,
+            source,
+            destination,
+            transferring: transferring_partition,
+            nonce,
+            statement,
+        },
+    )
+    .await
+    {
+        Err(e) => todo!("{e:?}"),
+        Ok(TransferInResponse::Ok(index)) => index,
+        Ok(r) => todo!("{r:?}"),
+    };
+
+    // TODO: This part is dangerous because TransferInRequest returns before
+    // the transfer has committed (for now). If that log entry doesn't commit
+    // and this calls CompleteTransferRequest, the keyspace will be lost
+    // forever.
+
+    let src_index = match rpc::send(
+        &agent_client,
+        source_leader,
+        CompleteTransferRequest {
+            realm,
+            source,
+            destination,
+            range,
+        },
+    )
+    .await
+    {
+        Err(e) => todo!("{e:?}"),
+        Ok(CompleteTransferResponse::Ok(index)) => index,
+        Ok(r) => todo!("{r:?}"),
+    };
+    // At this point the agents have queued the log entry that contained
+    // the completed transfer but may or may not have actually written
+    // it to the store yet. So we need to wait for that to happen.
+    // TODO: this should really wait for commit, and handled in the agent
+    // to be resolved with the overall transfer review/update.
+    let wait_for_entry = |group, index| async move {
+        loop {
+            if let Some(e) = store
+                .read_last_log_entry(&realm, &group)
+                .await
+                .expect("TODO")
+            {
+                if e.index >= index {
+                    return;
+                }
+                sleep(Duration::from_millis(1)).await;
+            }
+        }
+    };
+    wait_for_entry(source, src_index).await;
+    wait_for_entry(destination, dest_index).await;
+
+    Ok(())
+}
