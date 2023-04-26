09acc76b27219b6e1dd60417989fe6562bc61831
Diego Ongaro <diego@loam.me>
2023-04-25 19:00:05 -0700

    Add script to build and upload code to Google Cloud Storage
    Ansible in the infra repo will download it from GCS.
    
    Maybe someday we'd have CI run something like this.

---
+++ build-and-upload.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -eu
+
+PROJECT='loam-dev-env'
+BUCKET='a6cca386fe978990258c511c46fa8123a5f2e3b2' # sha1("daily-builds\n")
+MODE='release' # 'debug' or 'release'
+
+GIT="git-$(git describe --always --dirty)"
+TIMESTAMP="$(date --utc '+%Y%m%d%H%M%SZ')"
+GSPATH="$BUCKET/$USER/$TIMESTAMP/$GIT/$MODE"
+
+echo "Building version $GIT in $MODE mode"
+cargo build "--$MODE" \
+  --bin agent \
+  --bin cluster_manager \
+  --bin demo \
+  --bin http_hsm \
+  --bin load_balancer
+
+echo 'Uploading artifacts to Google Cloud Storage'
+gcloud storage cp \
+  "target/$MODE/agent" \
+  "target/$MODE/cluster_manager" \
+  "target/$MODE/demo" \
+  "target/$MODE/http_hsm" \
+  "target/$MODE/load_balancer" \
+  "gs://$GSPATH/"
+
+echo 'Upload done:'
+echo "https://console.cloud.google.com/storage/browser/$GSPATH?project=$PROJECT"
