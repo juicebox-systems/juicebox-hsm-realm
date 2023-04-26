0b97856a4436a842fced00595e4138433a812730
Diego Ongaro <diego@loam.me>
2023-04-26 11:33:24 -0700

    build-and-upload.sh: Print install URL if gcloud missing

--- build-and-upload.sh
+++ build-and-upload.sh
@@ -10,6 +10,11 @@ GIT="git-$(git describe --always --dirty)"
 TIMESTAMP="$(date -u '+%Y%m%d%H%M%SZ')"
 GSPATH="$BUCKET/$USER/$TIMESTAMP/$GIT/$MODE"
 
+if ! command -v gcloud > /dev/null; then
+  echo 'ERROR: Cannot find `gcloud`. See https://cloud.google.com/sdk/docs/install'
+  exit 1
+fi
+
 echo "Building version $GIT in $MODE mode"
 cargo build "--$MODE" \
   --bin agent \
