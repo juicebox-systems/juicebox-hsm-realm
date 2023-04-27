c132608852efd96b84f1d477fed4b5e7b959d17d
Diego Ongaro <diego@loam.me>
2023-04-26 19:51:09 -0700

    load-balancer: Split CertificateResolver into new file
    This commit changes no code other than imports.

--- load-balancer/src/main.rs
+++ load-balancer/src/main.rs
@@ -1,14 +1,9 @@
 use clap::Parser;
-use rustls::server::ResolvesServerCert;
-use rustls::sign::CertifiedKey;
-use rustls::{sign, Certificate, PrivateKey};
 use signal_hook::consts::signal::*;
 use signal_hook::iterator::Signals;
-use std::fs::File;
-use std::io::{self, BufReader};
 use std::net::SocketAddr;
-use std::path::{Path, PathBuf};
-use std::sync::{Arc, Mutex};
+use std::path::PathBuf;
+use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 use tracing::{info, warn};
@@ -19,8 +14,10 @@ use loam_mvp::logging;
 use loam_mvp::realm::store::bigtable::BigTableArgs;
 use loam_mvp::secret_manager::{Periodic, SecretManager, SecretsFile};
 
+mod cert;
 mod load_balancer;
 
+use cert::CertificateResolver;
 use load_balancer::LoadBalancer;
 
 #[derive(Parser)]
@@ -149,65 +146,4 @@ async fn main() {
 fn parse_listen(s: &str) -> Result<SocketAddr, String> {
     s.parse()
         .map_err(|e| format!("couldn't parse listen argument: {e}"))
-}
-
-struct CertificateResolver {
-    key_path: PathBuf,
-    cert_path: PathBuf,
-    current: Mutex<Arc<CertifiedKey>>,
-}
-
-impl CertificateResolver {
-    fn new(key_path: PathBuf, cert_path: PathBuf) -> Result<Self, Box<dyn std::error::Error>> {
-        let ck = Self::load(&key_path, &cert_path)?;
-        Ok(Self {
-            key_path,
-            cert_path,
-            current: Mutex::new(ck),
-        })
-    }
-
-    fn reload(&self) -> Result<(), Box<dyn std::error::Error>> {
-        let ck = Self::load(&self.key_path, &self.cert_path)?;
-        *self.current.lock().unwrap() = ck;
-        Ok(())
-    }
-
-    fn load(
-        key_path: &Path,
-        cert_path: &Path,
-    ) -> Result<Arc<CertifiedKey>, Box<dyn std::error::Error>> {
-        let certs = Self::load_certs(cert_path)?;
-        let keys = Self::load_keys(key_path)?;
-        let key = sign::any_supported_type(&keys[0])
-            .map_err(|_| rustls::Error::General("invalid private key".into()))?;
-        Ok(Arc::new(CertifiedKey::new(certs, key)))
-    }
-
-    fn load_certs(path: &Path) -> Result<Vec<Certificate>, Box<dyn std::error::Error>> {
-        let certs: Vec<_> = rustls_pemfile::certs(&mut BufReader::new(File::open(path)?))
-            .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid cert"))
-            .map(|certs| certs.into_iter().map(Certificate).collect())?;
-        if certs.is_empty() {
-            return Err("No certs found in file".into());
-        }
-        Ok(certs)
-    }
-
-    fn load_keys(path: &Path) -> Result<Vec<PrivateKey>, Box<dyn std::error::Error>> {
-        let keys: Vec<_> =
-            rustls_pemfile::pkcs8_private_keys(&mut BufReader::new(File::open(path)?))
-                .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid key"))
-                .map(|keys| keys.into_iter().map(PrivateKey).collect())?;
-        if keys.is_empty() {
-            return Err("No keys found in file".into());
-        }
-        Ok(keys)
-    }
-}
-
-impl ResolvesServerCert for CertificateResolver {
-    fn resolve(&self, _client_hello: rustls::server::ClientHello) -> Option<Arc<CertifiedKey>> {
-        Some(self.current.lock().unwrap().clone())
-    }
 }
