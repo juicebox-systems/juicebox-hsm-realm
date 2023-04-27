11a0e6995b0014d769dea09db3479ebb7ef26149
Diego Ongaro <diego@loam.me>
2023-04-26 20:35:25 -0700

    load-balancer: Include paths in CertificateResolver errors
    The errors from fs don't include path names, which makes it difficult to
    understand what's gone wrong.

--- load-balancer/src/cert.rs
+++ load-balancer/src/cert.rs
@@ -1,9 +1,9 @@
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
 use rustls::server::ResolvesServerCert;
 use rustls::sign::CertifiedKey;
 use rustls::{sign, Certificate, PrivateKey};
 use std::fs::File;
-use std::io::{self, BufReader};
+use std::io::BufReader;
 use std::path::{Path, PathBuf};
 use std::sync::{Arc, Mutex};
 
@@ -32,30 +32,33 @@ impl CertificateResolver {
     fn load(key_path: &Path, cert_path: &Path) -> Result<Arc<CertifiedKey>> {
         let certs = Self::load_certs(cert_path)?;
         let keys = Self::load_keys(key_path)?;
-        let key = sign::any_supported_type(&keys[0])
-            .map_err(|_| rustls::Error::General("invalid private key".into()))?;
+        let key = sign::any_supported_type(&keys[0]).context("invalid private key")?;
         Ok(Arc::new(CertifiedKey::new(certs, key)))
     }
 
     fn load_certs(path: &Path) -> Result<Vec<Certificate>> {
-        let certs: Vec<_> = rustls_pemfile::certs(&mut BufReader::new(File::open(path)?))
-            .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid cert"))
-            .map(|certs| certs.into_iter().map(Certificate).collect())?;
-        if certs.is_empty() {
-            return Err(anyhow!("No certs found in file"));
-        }
-        Ok(certs)
+        (|| {
+            let certs: Vec<_> = rustls_pemfile::certs(&mut BufReader::new(File::open(path)?))
+                .map(|certs| certs.into_iter().map(Certificate).collect())?;
+            if certs.is_empty() {
+                return Err(anyhow!("No certs found in file"));
+            }
+            Ok(certs)
+        })()
+        .with_context(|| format!("failed to read cert at {path:?}"))
     }
 
     fn load_keys(path: &Path) -> Result<Vec<PrivateKey>> {
-        let keys: Vec<_> =
-            rustls_pemfile::pkcs8_private_keys(&mut BufReader::new(File::open(path)?))
-                .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid key"))
-                .map(|keys| keys.into_iter().map(PrivateKey).collect())?;
-        if keys.is_empty() {
-            return Err(anyhow!("No keys found in file"));
-        }
-        Ok(keys)
+        (|| {
+            let keys: Vec<_> =
+                rustls_pemfile::pkcs8_private_keys(&mut BufReader::new(File::open(path)?))
+                    .map(|keys| keys.into_iter().map(PrivateKey).collect())?;
+            if keys.is_empty() {
+                return Err(anyhow!("No keys found in file"));
+            }
+            Ok(keys)
+        })()
+        .with_context(|| format!("failed to read keys at {path:?}"))
     }
 }
 
