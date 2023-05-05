4b4452d22072f63eb0a36a52b243d187a8df2e86
simon <simon@loam.me>
2023-05-05 12:04:14 -0700

    Allow build/test ppc scripts to use specific toolchain

--- test-ppc.sh
+++ test-ppc.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-
+if [ $# -ge 1 ]
+then
+    toolchain=$1; shift
+else
+    toolchain="nightly"
+fi
 # the e500mc is not the actual Entrust HSM CPU, but is similar enough (Qemu doesn't appear to know about the e5500)
-CARGO_TARGET_POWERPC_UNKNOWN_LINUX_GNU_RUNNER='qemu-ppc -cpu e500mc' QEMU_LD_PREFIX="/usr/powerpc-linux-gnu/" cargo +nightly test -p hsmcore --target powerpc-unknown-linux-gnu -Z build-std  "$@"
+CARGO_TARGET_POWERPC_UNKNOWN_LINUX_GNU_RUNNER='qemu-ppc -cpu e500mc' QEMU_LD_PREFIX="/usr/powerpc-linux-gnu/" cargo +${toolchain} test -p hsmcore --target powerpc-unknown-linux-gnu -Z build-std  "$@"
