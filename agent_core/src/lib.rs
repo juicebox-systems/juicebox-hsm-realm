e12cfff91b166cbbf1f48dd18d5dfde394b58d58
simon <simon@juicebox.me>
2023-06-23 09:36:59 -0700

    move append related items to own module

--- agent_core/src/lib.rs
+++ agent_core/src/lib.rs
@@ -2,7 +2,7 @@ use anyhow::Context;
 use bytes::Bytes;
 use futures::channel::oneshot;
 use futures::Future;
-use hsmcore::hsm::types::{AppRequestType, DataHash, LogEntry};
+use hsmcore::hsm::types::{AppRequestType, LogEntry};
 use http_body_util::Full;
 use hyper::server::conn::http1;
 use hyper::service::Service;
@@ -20,10 +20,12 @@ use tracing::{info, instrument, span, trace, warn, Instrument, Level, Span};
 use tracing_opentelemetry::OpenTelemetrySpanExt;
 use url::Url;
 
+mod append;
 mod commit;
 
 pub use juicebox_hsm::realm::agent::types;
 
+use append::{Append, AppendingState};
 use hsm_types::{
     CaptureNextRequest, CaptureNextResponse, Captured, EntryHmac, GroupId, HsmId, LogIndex,
     TransferInProofs,
@@ -33,7 +35,7 @@ use hsmcore::merkle::agent::{StoreDelta, TreeStoreError};
 use hsmcore::merkle::Dir;
 use juicebox_hsm::http_client::{Client, ClientOptions};
 use juicebox_hsm::logging::TracingSource;
-use juicebox_hsm::metrics::{self, Tag};
+use juicebox_hsm::metrics::{self};
 use juicebox_hsm::metrics_tag as tag;
 use juicebox_hsm::realm::hsm::client::{HsmClient, Transport};
 use juicebox_hsm::realm::merkle;
@@ -82,7 +84,7 @@ struct LeaderState {
 
     /// This serves as a mutex to prevent multiple concurrent appends to the
     /// store.
-    appending: AppendingState,
+    appending: append::AppendingState,
 
     /// A copy of the last log entry that was acknowledged by the store.
     ///
@@ -98,19 +100,6 @@ struct LeaderState {
     /// this log entry is complete.
     stepdown_at: Option<LogIndex>,
 }
-
-#[derive(Debug)]
-pub struct Append {
-    pub entry: LogEntry,
-    pub delta: StoreDelta<DataHash>,
-}
-
-#[derive(Debug)]
-enum AppendingState {
-    NotAppending { next: LogIndex },
-    Appending,
-}
-use AppendingState::{Appending, NotAppending};
 
 impl<T> Clone for Agent<T> {
     fn clone(&self) -> Self {
@@ -523,7 +512,7 @@ impl<T: Transport + 'static> Agent<T> {
             (realm, group),
             LeaderState {
                 append_queue: HashMap::new(),
-                appending: NotAppending {
+                appending: AppendingState::NotAppending {
                     next: starting_index.next(),
                 },
                 last_appended: None,
@@ -1337,126 +1326,5 @@ impl<T: Transport + 'static> Agent<T> {
             };
         }
         panic!("too slow to make progress");
-    }
-}
-
-impl<T: Transport + 'static> Agent<T> {
-    /// Precondition: agent is leader.
-    fn append(&self, realm: RealmId, group: GroupId, append_request: Append) {
-        let appending = {
-            let mut locked = self.0.state.lock().unwrap();
-            let leader = locked.leader.get_mut(&(realm, group)).unwrap();
-            let existing = leader
-                .append_queue
-                .insert(append_request.entry.index, append_request);
-            assert!(existing.is_none());
-            std::mem::replace(&mut leader.appending, Appending)
-        };
-
-        if let NotAppending { next } = appending {
-            let agent = self.clone();
-
-            tokio::spawn(async move { agent.keep_appending(realm, group, next).await });
-        }
-    }
-
-    const MAX_APPEND_BATCH_SIZE: usize = 100;
-
-    /// Precondition: `leader.appending` is Appending because this task is the one
-    /// doing the appending.
-    async fn keep_appending(&self, realm: RealmId, group: GroupId, next: LogIndex) {
-        let mut next = next;
-        let mut batch = Vec::new();
-        let metric_tags = [tag!(?realm), tag!(?group)];
-        let mut queue_depth: usize;
-
-        loop {
-            let mut delta = StoreDelta::default();
-            batch.clear();
-            {
-                let mut locked = self.0.state.lock().unwrap();
-                let Some(leader) = locked.leader.get_mut(&(realm, group)) else {
-                    return;
-                };
-                assert!(matches!(leader.appending, Appending));
-                while let Some(request) = leader.append_queue.remove(&next) {
-                    batch.push(request.entry);
-                    if delta.is_empty() {
-                        delta = request.delta;
-                    } else {
-                        delta.squash(request.delta);
-                    }
-                    next = next.next();
-                    if batch.len() >= Self::MAX_APPEND_BATCH_SIZE {
-                        break;
-                    }
-                }
-                if batch.is_empty() {
-                    leader.appending = NotAppending { next };
-                    return;
-                }
-                queue_depth = leader.append_queue.len();
-            }
-
-            let start = Instant::now();
-            match self.0.store.append(&realm, &group, &batch, delta).await {
-                Err(bigtable::AppendError::LogPrecondition) => {
-                    warn!(
-                        name = self.0.name,
-                        "detected dueling leaders, stepping down"
-                    );
-                    {
-                        let mut locked = self.0.state.lock().unwrap();
-                        // Empty the queue so we don't try and append anything else.
-                        if let Some(leader) = locked.leader.get_mut(&(realm, group)) {
-                            leader.append_queue.clear();
-                            leader.appending = NotAppending {
-                                next: LogIndex(u64::MAX),
-                            };
-                        }
-                    }
-                    self.handle_stepdown_as_leader(StepDownRequest { realm, group })
-                        .await
-                        .expect("error during leader stepdown");
-                    return;
-                }
-
-                Err(err) => todo!("{err:?}"),
-
-                Ok(()) => {
-                    let batch_size = batch.len();
-                    {
-                        let mut locked = self.0.state.lock().unwrap();
-                        if let Some(leader) = locked.leader.get_mut(&(realm, group)) {
-                            leader.last_appended = batch.pop();
-                        }
-                    }
-                    self.record_append_metrics(
-                        start.elapsed(),
-                        batch_size,
-                        queue_depth,
-                        &metric_tags,
-                    );
-                }
-            }
-        }
-    }
-
-    fn record_append_metrics(
-        &self,
-        elapsed: Duration,
-        batch_size: usize,
-        queue_depth: usize,
-        tags: &[Tag],
-    ) {
-        self.0.metrics.timing("bigtable.append.time", elapsed, tags);
-
-        self.0
-            .metrics
-            .histogram("bigtable.append.batch.size", batch_size, tags);
-
-        self.0
-            .metrics
-            .histogram("bigtable.append.queue.size", queue_depth, tags);
     }
 }
