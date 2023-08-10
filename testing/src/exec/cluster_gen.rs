9150ef04e0561ab468c68aa7c3d03bd35127d31a
Nora Trapp <nora@juicebox.me>
2023-08-10 14:16:12 -0700

    Fix tests

--- testing/src/exec/cluster_gen.rs
+++ testing/src/exec/cluster_gen.rs
@@ -17,6 +17,7 @@ use juicebox_realm_auth::creation::create_token;
 use juicebox_realm_auth::{AuthKey, AuthKeyVersion, Claims};
 use juicebox_sdk::{
     AuthToken, Client, ClientBuilder, Configuration, PinHashingMode, Realm, RealmId, TokioSleeper,
+    JUICEBOX_VERSION_HEADER, VERSION,
 };
 
 use super::bigtable::BigtableRunner;
@@ -118,6 +119,10 @@ impl ClusterResult {
             .tokio_sleeper()
             .reqwest_with_options(reqwest::ClientOptions {
                 additional_root_certs: vec![self.lb_cert()],
+                default_headers: HashMap::from([
+                    (JUICEBOX_VERSION_HEADER, VERSION),
+                    ("User-Agent", &format!("JuiceboxSdk-RustDemo/{}", VERSION)),
+                ]),
                 ..reqwest::ClientOptions::default()
             })
             .build()
