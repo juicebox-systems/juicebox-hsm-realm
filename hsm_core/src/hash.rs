1e4215e8d15b8cfd66d11f15ac476e786089e9fa
Diego Ongaro <diego@juicebox.me>
2023-08-21 13:24:27 -0700

    Replace ahash with Blake2 for HSM hash tables
    ahash didn't seem particularly appropriate or trustworthy for our use
    case. I had initially used ahash because it's the current hashbrown
    default, but it's not the std default.

--- hsm_core/src/hash.rs
+++ hsm_core/src/hash.rs
@@ -14,12 +14,23 @@
 //!
 //! [`NotRandomized`] does not mitigate HashDoS attacks, but it requires the
 //! user to be explicit in a way that is visible and grep-able in the code.
+//!
+//! ## Hash Function
+//!
+//! The hash function used is Blake2 (specifically [`Blake2sMac256`] for
+//! randomized hash tables and [`Blake2s256`] for non-randomized hash tables).
+//! The std hash tables currently use SipHash-1-3, but the SIP implementation
+//! is not officially maintained as a separate library. We chose Blake2 because
+//! this project already uses and trusts that dependency. It's a conservative
+//! choice which may impact performance.
 
 extern crate alloc;
 
 use alloc::sync::Arc;
+use blake2::{Blake2s256, Blake2sMac256};
 use core::hash::BuildHasher;
 use core::ops::DerefMut;
+use digest::{Digest, FixedOutput, KeyInit};
 use spin::once::Once;
 use spin::Mutex;
 
@@ -74,11 +85,10 @@ impl<K, S: Default> HashExt for HashSet<K, S> {
 pub struct NotRandomized;
 
 impl BuildHasher for NotRandomized {
-    type Hasher = ahash::AHasher;
+    type Hasher = Hasher<Blake2s256>;
 
     fn build_hasher(&self) -> Self::Hasher {
-        let state = ahash::RandomState::with_seeds(1, 2, 3, 4);
-        state.build_hasher()
+        Hasher(<Blake2s256 as Digest>::new())
     }
 }
 
@@ -100,24 +110,37 @@ impl BuildHasher for NotRandomized {
 /// let map: HashMap<String, String> = HashMap::new();
 /// ```
 #[derive(Clone, Debug)]
-pub struct RandomState(ahash::RandomState);
+pub struct RandomState(Blake2sMac256);
 
 impl RandomState {
     pub fn new(mut rng: impl CryptoRng) -> Self {
-        Self(ahash::RandomState::with_seeds(
-            rng.next_u64(),
-            rng.next_u64(),
-            rng.next_u64(),
-            rng.next_u64(),
-        ))
+        let mut key = [0u8; 32];
+        rng.fill_bytes(&mut key);
+        Self(<Blake2sMac256 as KeyInit>::new(&key.into()))
     }
 }
 
 impl BuildHasher for RandomState {
-    type Hasher = ahash::AHasher;
+    type Hasher = Hasher<Blake2sMac256>;
 
     fn build_hasher(&self) -> Self::Hasher {
-        self.0.build_hasher()
+        Hasher(self.0.clone())
+    }
+}
+
+/// A [`core::hash::Hasher`] implemented with the digest (rust-crypto) traits.
+///
+/// This allows cryptographic hash functions to be used for hash maps.
+pub struct Hasher<D: Clone + FixedOutput>(D);
+
+impl<D: Clone + FixedOutput> core::hash::Hasher for Hasher<D> {
+    fn write(&mut self, bytes: &[u8]) {
+        self.0.update(bytes)
+    }
+
+    fn finish(&self) -> u64 {
+        let output = self.0.clone().finalize_fixed();
+        u64::from_be_bytes(<[u8; 8]>::try_from(&output[..8]).unwrap())
     }
 }
 
@@ -155,4 +178,90 @@ pub fn set_global_rng_shared(rng: Arc<Mutex<dyn CryptoRng>>) {
 /// provided RNG.
 pub fn set_global_rng_owned(rng: impl CryptoRng + 'static) {
     GLOBAL_RNG.call_once(|| Arc::new(Mutex::new(rng)));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{HashExt, HashMap, HashSet, NotRandomized, RandomState};
+    use core::hash::{BuildHasher, Hasher};
+
+    #[test]
+    fn test_hash_map_randomized() {
+        // We don't need to register the global RNG here because tests default
+        // to using OsRng.
+        let mut map = HashMap::<u8, &'static str>::new();
+        map.insert(1, "one");
+        map.insert(2, "two");
+        assert_eq!(Some(&"one"), map.get(&1));
+        assert_eq!(Some(&"two"), map.get(&2));
+        assert_eq!(None, map.get(&3));
+    }
+
+    #[test]
+    fn test_hash_map_not_randomized() {
+        let mut map = HashMap::<u8, &'static str, NotRandomized>::new();
+        map.insert(1, "one");
+        map.insert(2, "two");
+        assert_eq!(Some(&"one"), map.get(&1));
+        assert_eq!(Some(&"two"), map.get(&2));
+        assert_eq!(None, map.get(&3));
+    }
+
+    #[test]
+    fn test_hash_set_randomized() {
+        // We don't need to register the global RNG here because tests default
+        // to using OsRng.
+        let mut set = HashSet::<u8>::new();
+        set.insert(1);
+        set.insert(2);
+        assert!(set.contains(&1));
+        assert!(set.contains(&2));
+        assert!(!set.contains(&3));
+    }
+
+    #[test]
+    fn test_hash_set_not_randomized() {
+        let mut set = HashSet::<u8>::new();
+        set.insert(1);
+        set.insert(2);
+        assert!(set.contains(&1));
+        assert!(set.contains(&2));
+        assert!(!set.contains(&3));
+    }
+
+    #[test]
+    fn test_random_state() {
+        // We don't need to register the global RNG here because tests default
+        // to using OsRng.
+        let build_hasher1 = RandomState::default();
+        let mut hasher1 = build_hasher1.build_hasher();
+        hasher1.write_u8(42);
+        hasher1.write_u8(11);
+
+        let mut hasher2 = build_hasher1.build_hasher();
+        hasher2.write_u8(42);
+        hasher2.write_u8(11);
+        assert_eq!(hasher1.finish(), hasher2.finish());
+
+        let build_hasher2 = RandomState::default();
+        let mut hasher3 = build_hasher2.build_hasher();
+        hasher3.write_u8(42);
+        hasher3.write_u8(11);
+        assert_ne!(hasher1.finish(), hasher3.finish());
+    }
+
+    #[test]
+    fn test_not_randomized() {
+        let mut hasher1 = NotRandomized.build_hasher();
+        assert_eq!(7575470396830417044, hasher1.finish());
+        hasher1.write_u8(42);
+        assert_eq!(5509661675087876720, hasher1.finish());
+        hasher1.write_u8(11);
+        assert_eq!(16522658520095206419, hasher1.finish());
+
+        let mut hasher2 = NotRandomized.build_hasher();
+        hasher2.write_u8(42);
+        hasher2.write_u8(11);
+        assert_eq!(16522658520095206419, hasher2.finish());
+    }
 }
