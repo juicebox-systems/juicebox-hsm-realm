f21a2e626221d0d5a2f7ec9fada1859ed8954f2c
simon <simon@juicebox.me>
2023-09-07 15:51:40 -0700

    remove bindgen output, have it generated on demand by build

--- entrust_nfast/build.rs
+++ entrust_nfast/build.rs
@@ -1,5 +1,64 @@
-// This is a dummy build.rs its required because we want to use the links
-// attribute in Cargo.toml The actual linking info is instead specified in
-// ~/.cargo/config.toml
-//
-fn main() {}
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // Tell cargo to look for shared libraries in the specified directory
+    println!("cargo:rustc-link-search=/opt/nfast/c/csd/gcc/lib/");
+
+    // Tell cargo to tell rustc to link the entrust libraries.
+    for lib in [
+        "rqcard", "nfkm", "nfstub", "nflog", "cutils", "rqcard", "nfkm", "nfstub", "nflog",
+        "cutils",
+    ] {
+        println!("cargo:rustc-link-lib={lib}");
+    }
+
+    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    println!("cargo:rerun-if-changed=src/bindgen.h");
+
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate bindings for.
+        .header("src/bindgen.h")
+        .use_core()
+        .prepend_enum_name(false)
+        .derive_default(true)
+        .impl_debug(true)
+        .generate_comments(false)
+        .allowlist_function("NF.*(Init|Connect|Disconnect|Transact|Submit|Query|Wait|Free_Reply|FreeACL|Lookup)")
+        .allowlist_function("NFKM_(findkey|freekey|recordkey|cmd_loadblob|getinfo|.*newkey.*|loadadminkeys.*|cert.*|getusablemodule)")
+        .allowlist_function("RQCard.*")
+        .allowlist_var("Cmd_CreateSEEWorld_Args.*")
+        .allowlist_var("Cmd_LoadBuffer.*")
+        .allowlist_var("Cmd_GenerateKey.*_flags.*")
+        .allowlist_var("NFastApp_ConnectionFlags.*")
+        .allowlist_var("Key_flags_.*")
+        .allowlist_var("Command_flags.*")
+        .allowlist_var("StatInfo_flags_.*")
+        .allowlist_var(".*_(enum)?table")
+        .allowlist_var(".*PermissionGroup.*")
+        .allowlist_var("Act_.*_Details_.*")
+        .allowlist_var("FileDevice.*")
+        .allowlist_var("NFKM_NKF_.*")
+        .allowlist_type("NFKM_(Admin.*|LoadAdminKeysHandle)")
+        .allowlist_type("M_SEEInitStatus")
+        .allowlist_type("M_KeyMgmtEntType")
+        .allowlist_type("M_StatNodeTag")
+        .clang_arg("-I/opt/nfast/c/csd/include-see/cutils/")
+        .clang_arg("-I/opt/nfast/c/csd/gcc/include/cutils/")
+        .clang_arg("-I/opt/nfast/c/csd/gcc/include/hilibs")
+        .clang_arg("-I/opt/nfast/c/csd/gcc/include/sworld/")
+
+        // Tell cargo to invalidate the built crate whenever any of the included
+        // header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
