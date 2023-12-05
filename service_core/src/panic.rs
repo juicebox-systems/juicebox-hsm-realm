87254bdc948b266f6d5ba5ceaf2a892ff7d87fa0
Diego Ongaro <diego@juicebox.me>
2023-12-05 12:03:51 -0800

    Add big panic header when output is going to a terminal
    This makes the panics easier to spot.

--- service_core/src/panic.rs
+++ service_core/src/panic.rs
@@ -1,11 +1,17 @@
+use std::io::IsTerminal;
 use std::panic::{set_hook, take_hook};
 use std::process::abort;
 
-/// Set's a panic hook that will perform the standard panic behavior and then
-/// exit the process.
+/// Sets a panic hook that will print a "PANIC" header in red text (if stderr
+/// is a terminal), perform the standard panic behavior, and then exit the
+/// process.
 pub fn set_abort_on_panic() {
+    let is_terminal = std::io::stderr().is_terminal();
     let default_panic = take_hook();
     set_hook(Box::new(move |info| {
+        if is_terminal {
+            eprintln!("\x1b[1;31m=== PANIC (exiting) ===\x1b[0m");
+        }
         default_panic(info);
         abort();
     }));
