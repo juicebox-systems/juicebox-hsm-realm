c39f43289cb2bbb37021a997269b7c7d2b9eacec
Diego Ongaro <diego@juicebox.me>
2024-01-03 18:09:08 -0800

    Add compaction test that pauses the leader

--- testing/tests/compaction.rs
+++ testing/tests/compaction.rs
@@ -5,10 +5,12 @@ use std::path::PathBuf;
 use std::process::Command;
 use std::time::Duration;
 use testing::background::BackgroundClientRequests;
+use tokio::sync::Semaphore;
 use tokio::time::sleep;
 use tracing::info;
 
 use juicebox_process_group::ProcessGroup;
+use juicebox_sdk::{Pin, Policy, UserInfo, UserSecret};
 use testing::exec::bigtable::emulator;
 use testing::exec::cluster_gen::{create_cluster, ClusterConfig, RealmConfig};
 use testing::exec::hsm_gen::Entrust;
@@ -17,15 +19,14 @@ use testing::exec::PortIssuer;
 // rust runs the tests in parallel, so we need each test to get its own port.
 static PORT: Lazy<PortIssuer> = Lazy::new(|| PortIssuer::new(8333));
 
-#[tokio::test(flavor = "multi_thread")]
-async fn compaction() {
-    // This panics if called from more than one test, but it can be helpful.
-    // observability::logging::configure("compaction test");
+// Only one cluster can be started at a time because the load balancer certs
+// are written out to a common filesystem path.
+//
+// TODO: fix that and remove this.
+static INIT_SEMAPHORE: Lazy<Semaphore> = Lazy::new(|| Semaphore::new(1));
 
-    let bt_args = emulator(PORT.next());
-    let mut processes = ProcessGroup::new();
-
-    let cluster_args = ClusterConfig {
+fn cluster_args() -> ClusterConfig {
+    ClusterConfig {
         load_balancers: 1,
         cluster_managers: 1,
         realms: vec![RealmConfig {
@@ -33,28 +34,37 @@ async fn compaction() {
             groups: 1,
             state_dir: None,
         }],
-        bigtable: bt_args,
+        bigtable: emulator(PORT.next()),
         local_pubsub: true,
         secrets_file: Some(PathBuf::from("../secrets-demo.json")),
         entrust: Entrust(false),
         path_to_target: PathBuf::from(".."),
-    };
+    }
+}
 
-    let cluster = create_cluster(cluster_args, &mut processes, 3000)
+#[tokio::test(flavor = "multi_thread")]
+async fn test_compaction_pause_witness() {
+    // This panics if called from more than one test, but it can be helpful.
+    // observability::logging::configure("compaction test");
+
+    let mut processes = ProcessGroup::new();
+
+    let init_permit = INIT_SEMAPHORE.acquire().await.unwrap();
+
+    let cluster = create_cluster(cluster_args(), &mut processes, PORT.clone())
         .await
         .unwrap();
+    let agents = &cluster.realms[0].agents;
 
     let client = cluster.client_for_user(String::from("teyla"));
     let mut background_work = BackgroundClientRequests::spawn(client).await;
-    let agents = &cluster.realms[0].agents;
 
     // Wait til our background register/recover workers have made some requests.
     background_work
         .wait_for_progress(3, Duration::from_secs(5))
         .await;
 
-    // Note: This test doesn't stop the leader because the background_work
-    // times out slowly and panics on any errors.
+    drop(init_permit);
 
     signal_agent(&agents[1], Signal::Stop);
 
@@ -81,6 +91,54 @@ async fn compaction() {
     background_work
         .wait_for_progress(3, Duration::from_secs(5))
         .await;
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_compaction_pause_leader() {
+    let init_permit = INIT_SEMAPHORE.acquire().await.unwrap();
+
+    let mut processes = ProcessGroup::new();
+    let cluster = create_cluster(cluster_args(), &mut processes, PORT.clone())
+        .await
+        .unwrap();
+    let agents = &cluster.realms[0].agents;
+
+    let client = cluster.client_for_user(String::from("teyla"));
+    let make_request = || async {
+        let pin = Pin::from(vec![1, 2, 3, 4]);
+        let info = UserInfo::from(vec![4, 3, 2, 1]);
+        client
+            .register(
+                &pin,
+                &UserSecret::from(b"secret".to_vec()),
+                &info,
+                Policy { num_guesses: 42 },
+            )
+            .await
+    };
+
+    make_request().await.unwrap();
+    drop(init_permit);
+
+    signal_agent(&agents[0], Signal::Stop);
+
+    info!("sleeping");
+    sleep(Duration::from_secs(10)).await;
+    make_request().await.unwrap();
+
+    signal_agent(&agents[0], Signal::Cont);
+    signal_agent(&agents[1], Signal::Stop);
+    // Now agents[0] will need to participate, since agents[1] is offline.
+
+    info!("sleeping");
+    sleep(Duration::from_secs(10)).await;
+    make_request().await.unwrap();
+
+    signal_agent(&agents[1], Signal::Cont);
+
+    info!("sleeping");
+    sleep(Duration::from_secs(10)).await;
+    make_request().await.unwrap();
 }
 
 #[derive(Clone, Copy, Debug)]
