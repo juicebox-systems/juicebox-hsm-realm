ae7c26b371aa37215b393b5d3b5d75f6d9570ceb
simon <simon@juicebox.me>
2024-03-11 08:54:58 -0700

    update script for build folder changes

--- build-and-upload.sh
+++ build-and-upload.sh
@@ -3,23 +3,22 @@
 # does a build optionally including the entrust components. Uploads the
 # results to a google cloud bucket from where it can be deployed.
 #
-# build-and-upload [-e <hsm-host>]
+# build-and-upload [-e]
 #
 # with no args, builds the standard components
-# with -e <hsm-host> will build & sign the entrust components as well.
+# with -e will build the entrust components as well.
 #
-# e.g. to use agent-2 in the colo to sign the code.
-# ./build-and-upload.sh -e agent@100.79.214.29
+# ./build-and-upload.sh -e
 #
 set -eu
 
 ENTRUST=0
 MODE='release' # or 'debug'
 
-while getopts e: FLAG
+while getopts :e FLAG
 do
   case "${FLAG}" in
-    e) REMOTE_HOST="${OPTARG}"; ENTRUST=1 ;;
+    e) ENTRUST=1 ;;
     *) exit 1 ;;
   esac
 done
@@ -27,9 +26,8 @@ done
 PROJECT='juicebox-infra'
 BUCKET='9c39e3ca5fca69f058b9e673aef193b10d9e8c48' # sha1("ci-builds\n")
 
-GIT="git-$(git describe --always --dirty)"
-TIMESTAMP="$(date -u '+%Y%m%d%H%M%SZ')"
-GSPATH="$BUCKET/$TIMESTAMP/$GIT/$MODE"
+GIT=`git rev-parse HEAD`
+GSPATH="$BUCKET/$GIT"
 
 if ! command -v gcloud > /dev/null; then
   echo "ERROR: Cannot find 'gcloud'. See https://cloud.google.com/sdk/docs/install"
@@ -38,7 +36,8 @@ fi
 
 echo "Building version $GIT in $MODE mode"
 if [ "$ENTRUST" -eq "1" ]; then
-  scripts/build-entrust-for-remote.sh "$REMOTE_HOST"
+  cargo build --all "--$MODE"
+  entrust_hsm/compile_linux.sh --features insecure
 else
   cargo build "--$MODE"
 fi
@@ -60,9 +59,7 @@ if [ "$ENTRUST" -eq "1" ]; then
     "target/$MODE/entrust_agent" \
     "target/$MODE/entrust_init" \
     "target/$MODE/entrust_ops" \
-    "target/powerpc-unknown-linux-gnu/$MODE/"entrust_signed_by_* \
-    "target/powerpc-unknown-linux-gnu/$MODE/entrust_hsm.sar" \
-    "target/powerpc-unknown-linux-gnu/$MODE/userdata.sar" \
+    "target/powerpc-unknown-linux-gnu/$MODE/entrust_hsm.elf" \
     "gs://$GSPATH/"
 fi
 
